07a0982eff9109bdb4079279222179ea296c5d98
mohammed <mohammed@10.1.217.251>
2015-02-25 20:25:51 -0500

    homework1

---
+++ src/hwstringmani/HwStringManipulator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author mohammed
+ */
+package HwStringManipulator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mohammed
+ */
+public class HwStringManipulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your favorite city: ");
+
+        String name = scan.next();
+
+        System.out.println("Characters name's length: " + name.length());
+
+        System.out.println(name.toUpperCase());
+
+        System.out.println(name.toLowerCase());
+
+        System.out.println(name.charAt(0));
+
+        System.out.println();
+
+    }
+
+}
